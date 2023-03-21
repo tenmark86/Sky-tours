cfff3d880206ab0dcda8258a3c7f36e27fc21b44
tenmark86e <tenmark86@gmail.com>
2023-03-21 18:25:42 +0300

    added trips page

---
+++ src/front/js/pages/trips.jsx
@@ -0,0 +1,57 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../store/appContext";
+import { TripCard } from "../components/trip-card";
+
+export const Trips = () => {
+  const { store, actions } = useContext(Context);
+  const [tripsMap, setTripsMap] = useState("");
+
+  useEffect(() => {
+    actions.getTrips();
+  }, []);
+
+  useEffect(() => {
+    if (store.trips != undefined || store.trip.user != undefined) {
+      setTripsMap(
+        store.trips.map((trip, index) => {
+          if (trip.is_active == true) {
+            return (
+              <TripCard
+                key={index.toString()}
+                tripID={trip.id}
+                username={trip.traveler_name}
+                userpicture={trip.traveler_picture}
+                userID={trip.traveler_id}
+                country={trip.country}
+                cities={trip.cities}
+                startDate={trip.start_date}
+                endDate={trip.end_date}
+                activities={trip.activities}
+                partners={trip.partners}
+              />
+            );
+          }
+        })
+      );
+    }
+  }, [store.trips]);
+
+  return (
+    <div className="trips-background py-0">
+      <div className="trips-body container my-0 pb-2">
+        <h1 className="text-center pt-4">Last proposed trips</h1>
+        <div className="d-flex flex-column-reverse">
+          {tripsMap != "" ? (
+            tripsMap
+          ) : (
+            <div className="d-flex justify-content-center ">
+              <div className="spinner-border text-warning my-5" role="status">
+                <span className="sr-only">Loading...</span>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
